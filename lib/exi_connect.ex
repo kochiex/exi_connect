803820d4278159297171d60f9741473840dbd396
NISHIUCHI Kazuma <nishiuchi.kazuma@gmail.com>
2021-07-26 18:31:22 +0900

    Initial commit

---
+++ lib/exi_connect.ex
@@ -0,0 +1,18 @@
+defmodule ExiConnect do
+  @moduledoc """
+  Documentation for ExiConnect.
+  """
+
+  @doc """
+  Hello world.
+
+  ## Examples
+
+      iex> ExiConnect.hello
+      :world
+
+  """
+  def hello do
+    :world
+  end
+end
---
+++ test/exi_connect_test.exs
@@ -0,0 +1,8 @@
+defmodule ExiConnectTest do
+  use ExUnit.Case
+  doctest ExiConnect
+
+  test "greets the world" do
+    assert ExiConnect.hello() == :world
+  end
+end
